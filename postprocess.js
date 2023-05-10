ebfe506c875e725a193ce9489214fef6b51ff7ea
Nathan Vaughn <nvaughn51@gmail.com>
2023-05-09 20:09:15 -0500

    Use redirects for links

---
+++ postprocess.js
@@ -0,0 +1,63 @@
+const toml = require("@iarna/toml");
+const fs = require("fs");
+const path = require("path");
+
+var cf_redirects_file = path.join("public", "_redirects");
+
+function create_page_redirect(slug, redirect_url){
+    // realistically speaking, this doesn't do anything,
+    // but future insurance if I stop using Cloudflare pages
+
+    // https://gohugo.io/content-management/urls/#how-aliases-work
+    slug_dir = path.join("public", slug);
+
+    // make the directory if it does not exist
+    if (!fs.existsSync(slug_dir)){
+        fs.mkdirSync(slug_dir);
+    }
+
+    fs.writeFileSync(path.join(slug_dir, "index.html"),
+    `<!DOCTYPE html>
+    <html lang="en-us">
+      <head>
+        <title>${redirect_url}</title>
+        <link rel="canonical" href="${redirect_url}">
+        <meta name="robots" content="noindex">
+        <meta charset="utf-8">
+        <meta http-equiv="refresh" content="0; url=${redirect_url}">
+      </head>
+    </html>`)
+}
+
+function create_cf_redirect(slug, redirect_url){
+    fs.appendFileSync(cf_redirects_file, `/${slug} ${redirect_url} 302\n`)
+}
+
+// delete existing redirects file
+if (fs.existsSync(cf_redirects_file)){
+    fs.rmSync(cf_redirects_file);
+}
+
+// parse the toml file
+var hugo_config = toml.parse(fs.readFileSync("hugo.toml"));
+var all_links = hugo_config.author.links.concat(hugo_config.author.nonrender_links);
+
+all_links.forEach(element => {
+    // get the url slug
+    var slug = Object.keys(element)[0];
+
+    // get the data for it
+    var data = element[slug];
+
+    // if it's just a string, use that, otherwise use the value from the "href" key.
+    if (typeof data == "string") {
+        var redirect_url = data;
+    } else {
+        var redirect_url = data.href;
+    }
+
+    console.log(`Creating redirect for /${slug} to ${redirect_url}`);
+
+    create_page_redirect(slug, redirect_url);
+    create_cf_redirect(slug, redirect_url);
+});
